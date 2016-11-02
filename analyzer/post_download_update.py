e3e1181da1158826b2cc9dea9f549f553152608d
Nicky Semenza <nicky@nickysemenza.com>
2016-11-01 22:24:14 -0400

    rename empty chats with participant names

--- analyzer/post_download_update.py
+++ analyzer/post_download_update.py
@@ -5,7 +5,7 @@ from sqlalchemy import create_engine, update
 # import pymysql.cursors
 load_dotenv(find_dotenv())
 import logging
-
+import json
 #Connect to MySQL
 engine = create_engine("mysql+pymysql://"
 	+os.environ.get("mysql_user")
@@ -15,11 +15,31 @@ engine = create_engine("mysql+pymysql://"
 
 conn = engine.connect()
 
-for x in conn.execute("""SELECT DISTINCT thread_id,
-                         COUNT(thread_id) AS subtotal
-                         FROM facebook_messages
-                         GROUP BY thread_id
-                         ORDER BY subtotal DESC
-                      """).fetchall():
-    conn.execute("UPDATE `facebook_threads` SET `downloaded_message_count` = '"+str(x['subtotal'])+"' WHERE `thread_id` = '"+str(x['thread_id'])+"'");
-print "downloaded_message_count updated!"
+# for x in conn.execute("""SELECT DISTINCT thread_id,
+#                          COUNT(thread_id) AS subtotal
+#                          FROM facebook_messages
+#                          GROUP BY thread_id
+#                          ORDER BY subtotal DESC
+#                       """).fetchall():
+#     conn.execute("UPDATE `facebook_threads` SET `downloaded_message_count` = '"+str(x['subtotal'])+"' WHERE `thread_id` = '"+str(x['thread_id'])+"'");
+# print "downloaded_message_count updated!"
+
+def lookupUserByFbId(id):
+    try: 
+        return conn.execute("SELECT full_name from facebook_users WHERE facebook_id = "+str(id)).fetchall()[0][0].replace("'", "")
+    except IndexError:
+        return "n/a"
+
+
+for x in conn.execute("SELECT thread_id, name, participant_ids from facebook_threads").fetchall():
+    if(x['name']==""):
+        people_ids = json.loads(x['participant_ids'])
+        people_names = []
+        #print json.dumps(people, sort_keys=True,indent=4, separators=(',', ': '))
+        for pid in people_ids:
+            # print pid
+            people_names.append(lookupUserByFbId(pid))
+            # print lookupUserByFbId(pid)
+        namestring = ",".join(people_names)
+        conn.execute("UPDATE `facebook_threads` SET `name` = '"+namestring+"' WHERE `thread_id` = '"+str(x['thread_id'])+"'");
+
