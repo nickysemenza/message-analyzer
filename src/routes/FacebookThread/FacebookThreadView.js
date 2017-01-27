f68bef6bf48dde6d5dfbf7bd982cf7ddc844ffda
Nicky Semenza <nicky@nickysemenza.com>
2017-01-27 02:57:26 -0500

    NVD3 test

--- src/routes/FacebookThread/FacebookThreadView.js
+++ src/routes/FacebookThread/FacebookThreadView.js
@@ -1,49 +1,43 @@
 import React, { Component, PropTypes } from 'react';
 import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
-// let PieChart = require("react-chartjs").Pie;
-import { Pie } from 'react-chartjs-2';
-
+import NVD3Chart from 'react-nvd3';
 export default class FacebookThreadView extends Component {
   componentDidMount () {
     this.props.loadData();
   }
   render () {
 
-    const randomColorGenerator = function () {
-      return '#' + (Math.random().toString(16) + '0000000').slice(2, 8);
-    };
-
-
-
     const threadObj = this.props.thread ? this.props.thread[this.props.thread_id] : null;
     let messages = threadObj && threadObj.messages ? threadObj.messages : [];
-
-
     let counts = threadObj && threadObj.stats ? threadObj.stats.counts : {};
-    let bg = [];
-    for (let i = 1; i <= Object.keys(counts).length; i++) {
-      bg.push(randomColorGenerator());
+
+    let data1 = [];
+    for(let c in counts) {
+      data1.push({key: c, y:counts[c]});
     }
-    const data = {
-      labels: Object.keys(counts),
-      datasets: [{data: Object.values(counts), backgroundColor: bg}]
-    };
-    console.log(data);
+
     return (<div>
         <h1>Viewing thread {this.props.thread_id}</h1>z
         {messages.length}
         <button onClick={this.props.loadData}>Reload Data</button>
 
         <div><pre>{JSON.stringify(threadObj ? threadObj.stats : [] , null, 2) }</pre></div>
-        {/*<PieChart data={counts} width="600" height="250"/>*/}
-        <Pie
-          data={data}
+
+        <NVD3Chart
+          tooltip={false}
+          id="chart"
+          width={900}
+          height={700}
+          type="pieChart"
+          datum={data1}
+          x="key"
+          y="y"
         />
 
         <BootstrapTable data={messages} striped={true} hover={true} pagination>
           <TableHeaderColumn dataField="id" width="40" isKey={true} dataSort={true}>ID</TableHeaderColumn>
-          <TableHeaderColumn dataField="thread_id" width="250" dataSort={true}>Thread ID</TableHeaderColumn>
-          <TableHeaderColumn dataField="sender_name" dataSort={true}>Sender Name</TableHeaderColumn>
+          <TableHeaderColumn dataField="thread_id" width="200" dataSort={true}>Thread ID</TableHeaderColumn>
+          <TableHeaderColumn dataField="sender_name" width="250" dataSort={true}>Sender Name</TableHeaderColumn>
           <TableHeaderColumn dataField="body" width="400" dataSort={true}>Message</TableHeaderColumn>
           <TableHeaderColumn dataField="raw" width="400" dataSort={true}>Raw</TableHeaderColumn>
         </BootstrapTable>
