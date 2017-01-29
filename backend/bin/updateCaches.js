760c544a34c206c6f0a32f51de8a0653fce127a7
Nicky Semenza <nicky@nickysemenza.com>
2017-01-29 03:14:07 -0500

    move scrapers

--- backend/bin/updateCaches.js
+++ backend/bin/updateCaches.js
@@ -2,28 +2,28 @@ const debug = require('debug')('express-sequelize');
 const models = require('../models');
 /* eslint-disable no-console */
 
-// models.sequelize.sync().then(() => {
+const countsQuery = 'SELECT DISTINCT thread_id, COUNT(thread_id) AS subtotal FROM facebook_messages GROUP BY thread_id ORDER BY subtotal DESC';
 
-  const countsQuery = 'SELECT DISTINCT thread_id, COUNT(thread_id) AS subtotal FROM facebook_messages GROUP BY thread_id ORDER BY subtotal DESC';
-  let updateChatCounts = new Promise((resolveO, reject) => {
-    models.sequelize.query(countsQuery).spread((results) => {
-      let promises = results.map(result=>{
-        return new Promise((resolve, reject) => {
-          models.FacebookThread.update(
-            { downloaded_message_count: result.subtotal },
-            { where: { thread_id: result.thread_id } }
-          ).then(updateResult => resolve(updateResult));
+
+  function updateChatCounts() {
+    return new Promise((resolveO, reject) => {
+      models.sequelize.query(countsQuery).spread((results) => {
+        let promises = results.map(result=>{
+          return new Promise((resolve, reject) => {
+            models.FacebookThread.update(
+              { downloaded_message_count: result.subtotal },
+              { where: { thread_id: result.thread_id } }
+            ).then(updateResult => resolve(updateResult));
+          });
         });
+        Promise.all(promises).then(p => {console.log('downloaded_message_count updated'); resolveO(p);});
       });
-      Promise.all(promises).then(p => {console.log('downloaded_message_count updated'); resolveO(p);});
+
     });
+  }
 
-  });
-
-
-
-  Promise.all([updateChatCounts]).then(() => {
+let chatCounts = updateChatCounts();
+  Promise.all([chatCounts]).then(() => {
     process.exit(0);
   });
 
-// });
--- backend/scraper/update_facebook_threads_list.js
+++
@@ -1,8 +0,0 @@
-let login = require("facebook-chat-api");
-let fs    = require("fs");
-let utils = require('./utils');
-
-login({appState: JSON.parse(fs.readFileSync('appstate.json', 'utf8'))}, (err, api) => {
-    if(err) return console.error(err);
-    utils.updateThreadsList(api, 0, 1000);
-});
