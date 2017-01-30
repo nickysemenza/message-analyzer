5138ea81521745c162aebf18266bf0b801dd0047
Nicky Semenza <nicky@nickysemenza.com>
2017-01-29 21:06:44 -0500

    pull stats from reds

--- backend/routes/facebook_threads.js
+++ backend/routes/facebook_threads.js
@@ -4,7 +4,12 @@ let router  = express.Router();
 
 let kue = require('kue')
   , queue = kue.createQueue();
+let redis = require("redis"),
+  client = redis.createClient();
+const bluebird = require('bluebird');
 
+bluebird.promisifyAll(redis.RedisClient.prototype);
+bluebird.promisifyAll(redis.Multi.prototype);
 
 router.get('/', (req, res) => {
   models.FacebookThread.findAll()
@@ -27,18 +32,21 @@ router.get('/:thread_id/messages', (req, res) => {
 });
 
 router.get('/:thread_id/stats', (req, res) => {
-  models.FacebookMessage.findAll({
-    where: {
-      thread_id: req.params.thread_id
-    }
-  }).then(thread => {
-    // let times = thread.map(item =>{ return {timestamp: item.timestamp, name: item.sender_name}});
-    let counts = {};
-    thread.forEach(msg => {
-      counts[msg.sender_name] = ++counts[msg.sender_name] || 1;
-    });
-    res.send({count: thread.length, counts});
-  });
+  client.hgetAsync("thread:"+req.params.thread_id,'stats').then(a=>{
+    res.send({counts: JSON.parse(a)});
+  })
+  // models.FacebookMessage.findAll({
+  //   where: {
+  //     thread_id: req.params.thread_id
+  //   }
+  // }).then(thread => {
+  //   // let times = thread.map(item =>{ return {timestamp: item.timestamp, name: item.sender_name}});
+  //   let counts = {};
+  //   thread.forEach(msg => {
+  //     counts[msg.sender_name] = ++counts[msg.sender_name] || 1;
+  //   });
+  //   res.send({count: thread.length, counts});
+  // });
 });
 
 module.exports =  router;
