3653dfcddbd58a382738bf7f4aa235680535230d
Nicky Semenza <nicky@nickysemenza.com>
2017-01-30 20:56:57 -0500

    Big table for thread list

--- src/routes/FacebookThread/FacebookThreadsList.js
+++ src/routes/FacebookThread/FacebookThreadsList.js
@@ -13,15 +13,32 @@ export default class FacebookThreadsList extends Component {
         {cell}
       </Link>
     );
-  }
+  };
   render () {
     let threadList = this.props.threads.threads ? this.props.threads.threads : [];
+
+    const options = {
+      sizePerPageList: [
+        {text: '100', value: 100},
+        {text: '500', value: 500},
+        {text: 'all', value: threadList.length}
+        ], // you can change the dropdown list for size per page
+      sizePerPage: 100  // which size per page you want to locate as default
+      // pageStartIndex: 0, // where to start counting the pages
+      // paginationSize: 3,  // the pagination bar size.
+      // prePage: 'Prev', // Previous page button text
+      // nextPage: 'Next', // Next page button text
+      // firstPage: 'First', // First page button text
+      // lastPage: 'Last', // Last page button text
+      // paginationShowsTotal: this.renderShowsTotal  // Accept bool or function
+      // hideSizePerPage: true > You can hide the dropdown for sizePerPage
+    };
 
     return (<div>
         <button onClick={this.props.loadData}>Reload Data</button>
         <button onClick={this.props.queueDownload}>Queue Thread List Download</button>
         <hr/>
-        <BootstrapTable data={threadList} striped={true} hover={true} pagination>
+        <BootstrapTable data={threadList} striped={true} hover={true} pagination options={options}>
           <TableHeaderColumn dataField="id" width="60" isKey={true} dataSort={true}>ID</TableHeaderColumn>
           <TableHeaderColumn dataField="thread_id" width="170" dataFormat={this.colFormatter} dataSort={true}>Thread ID</TableHeaderColumn>
           <TableHeaderColumn dataField="name" dataSort={true}>Thread Name</TableHeaderColumn>
