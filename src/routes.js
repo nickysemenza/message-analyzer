5a8dfa3c7917392f019a1d9c790873a58aeb8d8a
Nicky Semenza <nicky@nickysemenza.com>
2017-02-07 22:02:51 -0500

    Clean and run backend

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import App from './components/App';
-import HomePage from './components/HomePage';
-import AboutPage from './components/AboutPage';
 import FacebookThreadsListPage from './routes/FacebookThread/FacebookThreadsListPage';
 import FacebookThreadViewPage from './routes/FacebookThread/FacebookThreadViewPage';
 import FacebookUsersListPage from './routes/FacebookUsers/FacebookUsersListPage';
@@ -14,7 +12,6 @@ import NotFoundPage from './components/NotFoundPage';
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={DashboardPage}/>
-    <Route path="about" component={AboutPage}/>
     <Route path="facebook/threads" component={FacebookThreadsListPage}/>
     <Route path="facebook/threads/:thread_id/view" component={FacebookThreadViewPage}/>
     <Route path="facebook/users" component={FacebookUsersListPage}/>
